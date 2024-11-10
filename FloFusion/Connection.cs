e758d12bbabedffedf3ef2b50167e7cd90790a0a
LaCoDa08 <lacoda08@yahoo.com>
2024-11-09 22:48:30 -0600

    Add project files.

---
+++ FloFusion/Connection.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Threading.Tasks;
+using System.Data;
+
+
+namespace FloFusion
+{
+    internal class Connection
+    {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        string myConnectionString;
+        static string server = "localhost";
+        static string database = "flofusion";
+        static string uid = "root";
+        static string password = "PnckIs2Coo!";
+        public static string connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+        public bool OpenConnection()
+        {
+            conn = new MySqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server. Contact administrator");
+                        break;
+                    case 1045:
+                        Console.WriteLine("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+
+        }
+
+        public void CloseConnection()
+        {
+            conn.Close();
+            conn.Dispose();
+        }
+
+        public DataSet DataSet(string query)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                adapter.Fill(ds);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return ds;
+        }
+
+        public MySqlDataReader DataReader(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            return dataReader;
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            return cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ FloFusion/Form1.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FloFusion
+{
+    public partial class LoginForm : Form
+    {
+        Connection conn = new Connection();
+        string username, password;
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtUsername.Text != "" && txtPassword.Text != "")
+                {
+                    conn.OpenConnection();
+                    username = txtUsername.Text;
+                    password = txtPassword.Text;
+                    string query = "SELECT * FROM employee_account WHERE userName = '" + username + "' AND e_password = '" + password + "'";
+                    MySqlDataReader reader = conn.DataReader(query);
+
+                    if (reader.Read())
+                    {
+                        MessageBox.Show("Login Successful");
+                        this.Hide();
+                        MainForm mainForm = new MainForm();
+                        mainForm.Show();
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Username or Password");
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
